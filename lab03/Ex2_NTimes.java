ba3cd5c390a6b3d07dcc61781aafd145224b6837
caro <suoncaro@gmail.com>
2026-03-27 00:18:23 +0700

    renamed a lab

--- lab03/Ex2_NTimes.java
+++ lab03/Ex2_NTimes.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Lab032_NTimesInput {
+public class Ex2_NTimes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
