eedaed4c60c81a167fc63a3359422c6ae65548a9
caro <suoncaro@gmail.com>
2026-05-16 16:52:18 +0700

    Finished lab 6

--- lab06/Ex5/Ex5.java
+++ lab06/Ex5/Ex5.java
@@ -1,29 +1,117 @@
 package Ex5;
 
-import javax.swing.Timer;
-
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Ex5{
     public static void main(String[] args){
         JFrame frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JLabel label = new JLabel("wow: " + 1);
-        label.setBounds(20,10,100,10);
-        frame.add(label);
+        JLabel titleLabel = new JLabel("Calculator");
+        titleLabel.setBounds(20,20,100,20);
+        frame.add(titleLabel);
 
-        frame.setLayout(null);
-        frame.setSize(300,300);
-        frame.setVisible(true);
+        JLabel aLabel = new JLabel("A=");
+        aLabel.setBounds(20,40,100,20);
+        frame.add(aLabel);
 
-        Timer timer = new Timer(1, e -> {
-            int count = Integer.parseInt(label.getText().split(": ")[1]);
-            if(count < 1000){
-                label.setText("wow: " + (count + 1));
+        JTextField aField = new JTextField();
+        aField.setBounds(20,60,100,20);
+        frame.add(aField);
+
+        JLabel bLabel = new JLabel("B=");
+        bLabel.setBounds(20,80,100,20);
+        frame.add(bLabel);
+        
+        JTextField bField = new JTextField();
+        bField.setBounds(20,100,100,20);
+        frame.add(bField);
+
+        JButton addButton = new JButton("A+B");
+        addButton.setBounds(20,120,100,20);
+        frame.add(addButton);
+
+        JLabel addLabel = new JLabel("=");
+        addLabel.setBounds(20,140,100,20);
+        frame.add(addLabel);
+
+        JButton minusButton = new JButton("A-B");
+        minusButton.setBounds(20,160,100,20);
+        frame.add(minusButton);
+
+        JLabel minusLabel = new JLabel("=");
+        minusLabel.setBounds(20,180,100,20);
+        frame.add(minusLabel);
+
+        JButton multiplyButton = new JButton("A*B");
+        multiplyButton.setBounds(20,200,100,20);
+        frame.add(multiplyButton);
+
+        JLabel multiplyLabel = new JLabel("=");
+        multiplyLabel.setBounds(20,220,100,20);
+        frame.add(multiplyLabel);
+
+        JButton divisionButton = new JButton("A/B");
+        divisionButton.setBounds(20,240,100,20);
+        frame.add(divisionButton);
+
+        JLabel divisionLabel = new JLabel("=");
+        divisionLabel.setBounds(20,260,100,20);
+        frame.add(divisionLabel);
+
+        Calculator calculator = new Calculator();
+
+        // Action Listeneres
+        addButton.addActionListener(e -> {
+            try{
+                double a = Double.parseDouble(aField.getText()); 
+                double b = Double.parseDouble(bField.getText()); 
+
+                addLabel.setText("= " + calculator.add(a, b));
+            } catch (NumberFormatException err){
+                err.printStackTrace();
             }
         });
-        timer.start();
+
+        minusButton.addActionListener(e -> {
+                try{
+                double a = Double.parseDouble(aField.getText());
+                double b = Double.parseDouble(bField.getText());
+
+                minusLabel.setText("= "+calculator.subtract(a, b));
+            } catch (NumberFormatException err){
+                err.printStackTrace();
+            }
+        });
+
+        multiplyButton.addActionListener(e -> {
+            try{
+                double a = Double.parseDouble(aField.getText());
+                double b = Double.parseDouble(bField.getText());
+
+                multiplyLabel.setText("= "+calculator.multiply(a, b));
+            } catch (NumberFormatException err){
+                err.printStackTrace();
+            }
+        });
+
+        divisionButton.addActionListener(e -> {
+            try{
+                double a = Double.parseDouble(aField.getText());
+                double b = Double.parseDouble(bField.getText());
+
+                divisionLabel.setText("= " + calculator.divide(a, b));
+            } catch (NumberFormatException err) {
+                err.printStackTrace();
+            }
+        });
+
+        frame.setSize(300,400);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        return;
     }
 }
